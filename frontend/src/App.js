f05ffb6df75cb590ba677bd61d59e7d121d7616b
Himanshu Yadav <himanshu.yadav@hyperverge.co>
2021-01-23 05:43:12 +0530

    Add Navigation for searching and ticket status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,43 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
 import './App.css';
+import SideNavigation from './components/SideNavigation/SideNavigation';
+import SearchBus from './components/SearchBus/SearchBus';
+import AuthContext from './context/auth-context';
 
 const App = () => {
   return (
-    <div className="App black">
-      <div>Hello World</div>
-    </div>
+    // <AuthContext.Provider
+    //   value={{
+    //     isLoggedIn: isLoggedIn,
+    //     login: login,
+    //     logout: logout,
+    //     token: token,
+    //     userName: userName,
+    //   }}
+    // >
+    <Router>
+      <div className='App'>
+        <div style={{ display: 'flex' }}>
+          <SideNavigation />
+          <Switch>
+            <Route path='/search' exact>
+              <SearchBus />
+            </Route>
+            <Route path='/ticketStatus' exact>
+              <div className='white-text'>ticketstatus</div>
+            </Route>
+            <Redirect to='/' />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+    // </AuthContext.Provider>
   );
 };
 
---
+++ frontend/src/components/SearchBus/SearchBus.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './SearchBus.css';
+
+const SearchBus = (props) => {
+  return <div className='search-bus'>Piiiiink</div>;
+};
+
+export default SearchBus;
