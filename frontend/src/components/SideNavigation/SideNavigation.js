4cb213a3f5c6cb13a791a8cedf3a907e29061871
Himanshu Yadav <ydv1608@gmail.com>
2021-01-25 14:06:42 +0530

    Add auth context for app-wide auth, Add Bus Detail part for admin(remove bus seats functionality left)

--- frontend/src/components/SideNavigation/SideNavigation.js
+++ frontend/src/components/SideNavigation/SideNavigation.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {useState, useCallback, useContext} from 'react';
 import { NavLink } from 'react-router-dom';
 
 import './SideNavigation.css';
+import AuthContext from '../../context/auth-context';
 
 const SideNavigation = (props) => {
+  
+  const auth = useContext(AuthContext);
+  
+  const doLogOut = () => {
+    auth.logout();
+  }
+
   return (
     <div className='side-navigation'>
       <div className='app-header'>
@@ -20,13 +28,42 @@ const SideNavigation = (props) => {
         exact>
         Search Bus
       </NavLink>
-      <NavLink
-        activeClassName='active-nav'
-        className='NavLink'
-        to='/ticketStatus'
-        exact>
-        View Ticket Status
-      </NavLink>
+      {!auth.isLoggedIn && (
+        <React.Fragment>
+          <NavLink
+            activeClassName='active-nav'
+            className='NavLink'
+            to='/ticketStatus'
+            exact>
+            View Ticket Status
+          </NavLink>
+          <NavLink
+            activeClassName='active-nav'
+            className='NavLink'
+            to='/login'
+            exact>
+            Are you an Admin?
+        </NavLink>
+      </React.Fragment>
+      )}
+      {auth.isLoggedIn && (
+        <React.Fragment>
+          <NavLink
+            activeClassName='active-nav'
+            className='NavLink'
+            to='/addBus'
+            exact>
+            Add Bus
+          </NavLink>
+          <NavLink
+            onClick={doLogOut}
+            className='NavLink'
+            to='/search'
+            exact>
+            LogOut
+          </NavLink>
+        </React.Fragment>
+      )}
     </div>
   );
 };
